ce66e085308b616a10d10c07127857efa5154b5a
Diego Lopes <diego-chez@hotmail.com>
2020-08-01 17:29:54 -0300

    refactor: send httpStatus error message

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -3,32 +3,25 @@ const { environment } = require("../config/env");
 const { ValidationError } = require("express-validation");
 const ApiError = require("../utils/ApiError");
 
-/**
- *  Error handler. Send stacktrace when in development env
- *  @public
- * */
-const handler = (err, req, res, next) => {
-  const response = {
-    statusCode: err.statusCode,
-    errors: err.errors,
-    message: err.message || httpStatus[err.status],
-    stack: err.stack,
-    data: err.data,
-  };
-  if (environment !== "development") delete response.stack;
+function handler(err, req, res) {
+  const isProd = environment === "production";
+  const message = isProd ? httpStatus[err.statusCode] : err.message;
   const statusCode = err.statusCode
     ? err.statusCode
     : httpStatus.INTERNAL_SERVER_ERROR;
+
+  const response = {
+    statusCode: err.statusCode,
+    errors: err.errors,
+    message,
+    stack: isProd ? undefined : err.stack,
+    data: err.data,
+  };
   res.status(statusCode);
   res.json(response);
-};
-exports.handler = handler;
+}
 
-/**
- * If error is not an instanceOf Error, convert it.
- * @public
- * */
-exports.converter = (err, req, res, next) => {
+function converter(err, req, res, next) {
   let convertedError = err;
   if (err instanceof ValidationError) {
     const errors = {};
@@ -61,15 +54,18 @@ exports.converter = (err, req, res, next) => {
     });
   }
   return handler(convertedError, req, res);
-};
+}
 
-/**
- * Catch 404 and forward to error handler
- */
-exports.notFound = (req, res) => {
+function notFound(req, res) {
   const err = new ApiError({
     message: "Not found",
     statusCode: httpStatus.NOT_FOUND,
   });
   return handler(err, req, res);
+}
+
+module.exports = {
+  converter,
+  notFound,
+  handler,
 };
